53d99d1ede19a8181aa58f5005ec78c739dc2587
mjaumi <mjaumi2864@gmail.com>
2025-05-11 21:15:16 +0600

    email verification feature added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('verify/<uid64>/<token>/', views.verify_email, name='verify_email'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserRegistrationForm
 from .utils import send_verification_email
 from django.contrib import messages
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
-# Create your views here.
+from .models import CustomUser
+
+# user registration view
 def register(request):
     if request.method == 'POST':
         form = CustomUserRegistrationForm(request.POST)
@@ -19,6 +23,24 @@ def register(request):
             return render(request, 'register.html', {'form': form})
         
     return render(request, 'register.html')
+
+# user email verification view
+def verify_email(request, uid64, token):
+    try:
+        uid = urlsafe_base64_decode(uid64).decode()
+        user = CustomUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, CustomUser.DoesNotExist):
+        user = None
+
+    if user and default_token_generator.check_token(user, token):
+        user.is_verified = True
+        user.save()
+        messages.success(request, 'Email verified successfully! You can now log in.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid or expired verification link!')
+        return redirect('register')
+
 
 def login(request):
     return render(request, 'login.html')
