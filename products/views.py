24b518e34408b32b6fce8ab57b1ab893dda0700c
mjaumi <mjaumi2864@gmail.com>
2025-05-15 23:45:28 +0600

    featured product section design done

--- products/views.py
+++ products/views.py
@@ -4,7 +4,12 @@ from .models import Product
 # Home page view
 def home(request):
     products = Product.objects.all()
-    return render(request, 'home.html', {'products': products})
+    featured_products = Product.objects.filter(is_featured=True)
+
+    return render(request, 'home.html', {
+        'products': products,
+        'featured_products': featured_products
+    })
 
 # Product details view
 def product_details(request, product_id):
