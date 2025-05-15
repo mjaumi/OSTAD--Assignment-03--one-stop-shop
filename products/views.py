7ffec33a4c0402bfa0b4f61d81ab0e9e9ce944b9
mjaumi <mjaumi2864@gmail.com>
2025-05-15 23:10:43 +0600

    project details page design done

--- products/views.py
+++ products/views.py
@@ -5,3 +5,8 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+
+# Product details view
+def product_details(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, 'product_details.html', {'product': product})
